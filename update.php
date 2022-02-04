eedd2a7de89efc556c8265730c4f8ddf45c57c26
Florian Falk <dasnerdwork@gmail.com>
2022-02-04 02:28:03 +0100

    Error State 1

--- update.php
+++ update.php
@@ -20,21 +20,30 @@ function updateProfile($id){
     $jsonArray["MasteryData"] = $masteryData;
     $jsonArray["MatchIDs"] = $matchids;
     
-    
-    if(file_exists('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json')){
-        $existingJson =  file_get_contents('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json');
-    } else { $existingJson = ""; } // else empty $existingJson string so following if-statement forced into else part
-    
-    $fp = fopen('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json', 'c');
-    // Open the file only to write. If it doesnt exist it will be created. If it exists it won't be truncated (would result in permanent delete-create loop)
-    
-    if($existingJson == json_encode($jsonArray)){
-        // If current existing file equals the new downloaded array data do nothing
-    } else {
-        // Else update the current existing (or not existing) file with the newest data
-        fwrite($fp, json_encode($jsonArray));
-        fclose($fp);
-    }    
+    if($sumid != ""){
+        if(file_exists('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json')){
+            $existingJson =  file_get_contents('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json');
+        } else { $existingJson = ""; } // else empty $existingJson string so following if-statement forced into else part
+        
+        $fp = fopen('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json', 'c');
+        // Open the file only to write. If it doesnt exist it will be created. If it exists it won't be truncated (would result in permanent delete-create loop)
+        
+        if($existingJson == json_encode($jsonArray)){
+            // If current existing file equals the new downloaded array data do nothing
+        } else {
+            // Else update the current existing (or not existing) file with the newest data
+            fwrite($fp, json_encode($jsonArray));
+            fclose($fp);
+        }
+
+        $playerDataArray = json_decode(file_get_contents('/var/www/html/wordpress/clashapp/data/player/'.$sumid.'.json'), true);
+        foreach($playerDataArray["MatchIDs"] as $match){
+            if(!file_exists('/var/www/html/wordpress/clashapp/data/matches/'.$match.'.json')){
+                getMatchByID($match, $playerName);
+            }
+
+        }
+    }
 }
 
 ?>
