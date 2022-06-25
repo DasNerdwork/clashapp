2a7bae0ecae3fb598417694da5cea64c4445c274
Florian Falk <dasnerdwork@gmail.com>
2022-06-25 03:33:26 +0200

    ...

--- addToFile.php
+++ addToFile.php
@@ -4,17 +4,21 @@ $teamID = $_POST["teamid"];
 $suggestBanArray = array();
 if(file_exists('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json')){
     $suggestedBanFileContent = json_decode(file_get_contents('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json'), true);
-    $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
-    if($suggestedBanFileContent == json_encode($suggestBanArray)){
-        fclose($fp);
-    } else {
-        $suggestBanArray["SuggestedBans"] = $champname;
+    // if(in_array($suggestedBanFileContent["SuggestedBans"], $championname)){ // not working, no idea why
+
+    // } else {
         $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
-        fwrite($fp, json_encode($suggestBanArray));
-        fclose($fp);
-    }
+        if($suggestedBanFileContent["SuggestedBans"] == $champname){
+            fclose($fp);
+        } else {
+            $suggestedBanFileContent["SuggestedBans"][] = $champname;
+            // $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
+            fwrite($fp, json_encode($suggestedBanFileContent));
+            fclose($fp);
+        }
+    // }
 } else {
-    $suggestBanArray["SuggestedBans"] = $champname;
+    $suggestBanArray["SuggestedBans"][0] = $champname;
     $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
     fwrite($fp, json_encode($suggestBanArray));
     fclose($fp);
