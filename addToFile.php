f48a4df74fa31d976c25d645568c7a307533b680
Florian Falk <dasnerdwork@gmail.com>
2022-07-07 00:23:44 +0200

    Modified clash update from fpopen with c to w ?

--- addToFile.php
+++ addToFile.php
@@ -4,37 +4,48 @@ $champname = $_POST["champname"];
 $teamID = $_POST["teamid"];
 
 // $champid = "Aatrox";
-// $champname = "Aatrox";
+// $champname = "Aatrox";       // Example data used for testing
 // $teamID = "dasnerdwork";
 
-$suggestBanArray = array();
-
-if(file_exists('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json')){
-    $suggestedBanFileContent = json_decode(file_get_contents('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json'), true);
-    
-    if(array_search($champid, array_column($suggestedBanFileContent["SuggestedBans"], 'id')) !== false){
-        echo '{"status":"ElementAlreadyInArray"}';die();
-    } else if (count($suggestedBanFileContent["SuggestedBans"]) >= 10) {
-        echo '{"status":"MaximumElementsExceeded"}';die();
+$validChamps = json_decode(file_get_contents('/var/www/html/wordpress/clashapp/data/patch/12.12.1/data/de_DE/champion.json'), true)["data"];
+if($teamID == ""){
+    echo '{"status":"InvalidTeamID"}';die();
+} else {
+    $key = array_search($champid, array_column($validChamps, 'id'));
+    $key2 = array_search($champname, array_column($validChamps, 'name'));
+    if($key !== $key2 || $key === false || $key2 === false){
+        echo '{"status":"CodeInjectionDetected"}';die();
     } else {
-        echo '{"status":"Success"}';
-        $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
-        if($suggestedBanFileContent["SuggestedBans"] == $champid){
-            fclose($fp);
+        $suggestBanArray = array();
+
+        if(file_exists('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json')){
+            $suggestedBanFileContent = json_decode(file_get_contents('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json'), true);
+            
+            if(array_search($champid, array_column($suggestedBanFileContent["SuggestedBans"], 'id')) !== false){
+                echo '{"status":"ElementAlreadyInArray"}';die();
+            } else if (count($suggestedBanFileContent["SuggestedBans"]) >= 10) {
+                echo '{"status":"MaximumElementsExceeded"}';die();
+            } else {
+                echo '{"status":"Success"}';
+                $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
+                if($suggestedBanFileContent["SuggestedBans"] == $champid){
+                    fclose($fp);
+                } else {
+                    $suggestedBanFileContent["SuggestedBans"][] = array("id"=>$champid,"name"=>$champname);
+                    $suggestedBanFileContent["Status"]++;
+                    fwrite($fp, json_encode($suggestedBanFileContent));
+                    fclose($fp);
+                }
+            }
         } else {
-            $suggestedBanFileContent["SuggestedBans"][] = array("id"=>$champid,"name"=>$champname);
-            $suggestedBanFileContent["Status"]++;
-            fwrite($fp, json_encode($suggestedBanFileContent));
+            echo '{"status":"FileDoesNotExist"}';
+            $suggestBanArray["SuggestedBans"][] = array("id"=>$champid,"name"=>$champname);
+            $suggestBanArray["Status"] = 1;
+
+            $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
+            fwrite($fp, json_encode($suggestBanArray));
             fclose($fp);
         }
     }
-} else {
-    echo '{"status":"FileDoesNotExist"}';
-    $suggestBanArray["SuggestedBans"][] = array("id"=>$champid,"name"=>$champname);
-    $suggestBanArray["Status"] = 1;
-
-    $fp = fopen('/var/www/html/wordpress/clashapp/data/teams/'.$teamID.'.json', 'c');
-    fwrite($fp, json_encode($suggestBanArray));
-    fclose($fp);
 }
 ?>
--- updateTeam.php
+++ updateTeam.php
@@ -1,9 +1,19 @@
 <?php
 include_once('update.php');
 
+$id = 0;
+
 if(isset($_POST["usernames"])){
+    echo "{\"$id\":";
     foreach($_POST["usernames"] as $playerName){
-        updateProfile($_POST["usernames"], 15);      
+        // echo $playerName."<br>";
+        updateProfile($playerName, 15);    
+        sleep(1);
+        $id++;
+        if($id < count($_POST["usernames"])){
+            echo ", \"$id\":";
+        }
     }
+    echo "}";
 }
 ?>
