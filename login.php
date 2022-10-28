5c9c2796b010c65fb327327931f508b7c1b25b23
Florian Falk <dasnerdwork@gmail.com>
2022-10-28 04:45:56 +0200

    Added login, logout & registration functionalities also myaccount but only for "temporary" usage all via mysql/i and php

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+ 
+if (isset($_SESSION['user'])) {
+    header('Location: myaccount');
+}
+
+require_once 'clash-db.php';
+ 
+$error_message = '';
+if (isset($_POST['submit'])) {
+    $db = new DB();
+    $response = $db->check_credentials($_POST['mailorname'], $_POST['password']);
+
+    if ($response['status'] == 'success') {
+        $_SESSION['user'] = array('id' => $response['id'], 'username' => $response['username']);
+        header('Location: myaccount');
+    }
+ 
+    $error_message = ($response['status'] == 'error') ? $response['message'] : '';
+}
+
+?>
+ 
+<?php if (!empty($error_message)) { ?>
+    <div class="error">
+        <strong><?php echo $error_message; ?></strong>
+    </div>
+<?php } ?>
+<form method="post">
+    <p>
+        <label for="mailorname">Email/Username: </label>
+        <input type="text" name="mailorname" id="mailorname" placeholder="Enter Email or Username" required />
+    </p>
+    <p>
+        <label for="password">Password: </label>
+        <input type="password" name="password" id="password" placeholder="Enter Password" required />
+    </p>
+    <input type="submit" name="submit" value="Login" />
+</form>
