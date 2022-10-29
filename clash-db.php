d5fee229c085dff4ab43733a8bd58c002bb90d23
Florian Falk <dasnerdwork@gmail.com>
2022-10-29 16:47:18 +0200

    Added "unverified, please verify" if status = 2

--- clash-db.php
+++ clash-db.php
@@ -1,4 +1,5 @@
 <?php
+
 class DB {
     private $dbHost     = "***REMOVED***";
     private $dbUsername = "***REMOVED***";
@@ -21,9 +22,9 @@ class DB {
   
     public function check_credentials($mailorname = '', $password = '') {
         if(str_contains($mailorname, '@')){
-            $sql = $this->db->prepare("SELECT id, username, password, status FROM users WHERE email = ?");
+            $sql = $this->db->prepare("SELECT id, region, username, email, password, status FROM users WHERE email = ?");
         } else {
-            $sql = $this->db->prepare("SELECT id, username, password, status FROM users WHERE username = ?");
+            $sql = $this->db->prepare("SELECT id, region, username, email, password, status FROM users WHERE username = ?");
         }
         $sql->bind_param('s', $mailorname);
         $sql->execute();
@@ -31,27 +32,25 @@ class DB {
         
         if($result->num_rows) {
             
-            $entry = $result->fetch_assoc(); // Fetch returnvalue of mysql query above
+            $row = $result->fetch_assoc(); // Fetch returnvalue to an array ($row) of mysql query above
  
-            if ('1' == $entry['status']) {
-                if (password_verify($password, $entry['password'])) {
-                    return array('status' => 'success', 'id' => $entry['id'], 'username' => $entry['username']);
+            if ($row['status'] == '1' || $row['status'] == '2') {
+                if (password_verify($password, $row['password'])) {
+                    return array('status' => 'success', 'id' => $row['id'], 'region' => $row['region'], 'username' => $row['username'], 'email' => $row['email']);
                 }
                 return array('status' => 'error', 'message' => 'Email or password is invalid.'); // The Password decrypt was unsuccessful
             }
  
-            return array('status' => 'error', 'message' => 'Your account is not activated yet.'); // The Users status is set to 0 (deactivated account)
+            return array('status' => 'error', 'message' => 'Your account was deactivated. If you did not take this action please reach out to an administrator.'); // The Users status is set to 0 (deactivated account)
         }
         return array('status' => 'error', 'message' => 'The given account does not exist.'); // Cannot find email in database
     }
 
-    public function account_exists($email = '') {
-        $sql = $this->db->prepare("SELECT id FROM users WHERE email = ?");
-        $sql->bind_param('s', $email);
+    public function account_exists($email = '', $username = '') {
+        $sql = $this->db->prepare("SELECT id FROM users WHERE email = ? OR username = ?");
+        $sql->bind_param('ss', $email, $username);
         $sql->execute();
         $result = $sql->get_result();
-
-        // print_r($result);die();
 
         if($result->num_rows) {
             return true;
@@ -60,28 +59,60 @@ class DB {
         }
     } 
 
-// CREATE TABLE 'users' (  'id' int(11) NOT NULL AUTO_INCREMENT, 'username' varchar(255) NOT NULL, 'region' varchar(4) NOT NULL, 'email' varchar(255) NOT NULL,  'password' varchar(255) NOT NULL,  'status' int(1) NOT NULL DEFAULT 1 COMMENT '2=Unverified|1=Active|0=Inactive',  PRIMARY KEY ('id') ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
     public function create_account($username, $region, $email, $password, $verifier) {
-        //ALTER TABLE `personen` ADD `hash` VARCHAR(255) NOT NULL AFTER `fk_laden`;
-        $sql = $this->db->prepare("INSERT INTO users (username, region, email, password, verifier, status) VALUES (?, ?, ?, ?, ?, 2)"); // INSERT INTO 'users' ('username', 'email', 'password', 'status') VALUES ('John Doe', 'john.doe@example.com', MD5('123456'), 1), ('Sam Doe', 'sam.doe@example.com', MD5('123456'), 0);
+        $sql = $this->db->prepare("INSERT INTO users (username, region, email, password, verifier, status) VALUES (?, ?, ?, ?, ?, 2)"); 
         $sql->bind_param('sssss', $username, $region, $email, $password, $verifier);
         $sql->execute();
         $result = $sql->get_result();
-
-        print_r($sql);
 
         if(is_numeric($sql->insert_id)){
             return array('status' => 'success', 'message' => 'Account successfully created!', 'id' => $sql->insert_id);
         }else{
             return array('status' => 'error', 'message' => 'Unable to create account.');
         }
-        // print_r($result);die();
+    }
 
-        // if($result->num_rows) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
+    public function verify_account($verifier = '') {
+        $sql = $this->db->prepare("UPDATE users SET status = '1' WHERE verifier = ?");
+        $sql->bind_param('s', $verifier);
+        $sql->execute();
+        $result = $sql->affected_rows;
+
+        $sql = $this->db->prepare("UPDATE users SET verifier = NULL WHERE verifier = ?");
+        $sql->bind_param('s', $verifier);
+        $sql->execute();
+        $result2 = $sql->affected_rows;
+
+        if($result > 0 && $result2 > 0) {
+            return array('status' => 'success', 'message' => 'Account successfully verified! You may now <a href="/login">login</a>.');
+        } else {
+            return array('status' => 'error', 'message' => 'Unable to verify account.');
+        }
+    } 
+
+    public function check_status($id, $username) {
+        $sql = $this->db->prepare("SELECT status FROM users WHERE id = ? AND username = ?");
+        $sql->bind_param('ss', $id, $username);
+        $sql->execute();
+        $result = $sql->get_result();
+
+        if($result->num_rows) {
+            
+            $row = $result->fetch_assoc();
+
+            switch ($row['status']) {
+                case "0":
+                    return array('status' => 'deactivated', 'message' => 'This account has been deactivated.');
+                case "1":
+                    return array('status' => 'verified', 'message' => '');
+                case "2":
+                    return array('status' => 'unverified', 'message' => 'Your account has not been verified yet. Please check your mails (including spam folder) to be able to use all functionalities.');
+                default:
+                    return array('status' => 'unknown', 'message' => 'Unknown account status. Please contact an administrator.');
+            }
+        } else {
+            return array('status' => 'unknown', 'message' => 'Cannot find user in database. Please contact an administrator.');
+        }
     }
 }
 ?>
