d1a0375d56db73a3c1e1693e334c785b50f4d955
Florian Falk <dasnerdwork@gmail.com>
2022-12-13 23:47:41 +0100

    test

---
+++ accounts/reset.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+ 
+if (isset($_SESSION['user'])) {
+    header('Location: settings');
+}
+
+require_once '/hdd2/clashapp/clash-db.php';
+ 
+$return_message = '';
+
+if (isset($_GET["code"])) {
+    include('head.php');
+    setCodeHeader('Reset Password', true, true);
+    include('header.php');
+
+    $db = new DB();
+    $response = $db->check_reset_code($_GET["code"]);
+    $return_message = $response['message'];
+
+    if ($response['status'] == 'success') {
+        
+
+    if (!empty($return_message)) { ?>
+        <div class="error">
+            <strong><?php echo $return_message; ?></strong>
+        </div>
+   <?php 
+        } 
+        // echo "<div>Code for pw reset: ".$_GET["code"]."</div>";
+    ?>
+    <h1>Reset Password:</h1>
+        <form method="post">
+            <p>
+                <label for="password">Password: </label>
+                <input type="password" name="password" id="password" placeholder="Enter New Password" required />
+            </p>
+            <p>
+                <label for="password">Password: </label>
+                <input type="password" name="password-confirm" id="password-confirm" placeholder="Confirm New Password" required />
+            </p>
+            <input type="submit" name="submit" value="Reset Password" />
+        </form>
+    <?php
+    } else {
+        header('Location: 404');
+    }
+}
+
+if (isset($_POST['submit'])) {
+    $uppercase = preg_match('@[A-Z]@', $_POST['password']);
+    $lowercase = preg_match('@[a-z]@', $_POST['password']);
+    $number    = preg_match('@[0-9]@', $_POST['password']);
+    $specialChars = preg_match('@[^\w]@', $_POST['password']);
+    if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($_POST['password']) < 8 || strlen($_POST['password']) > 32) { // The Password meets current regex settings above
+        echo "<strong><div>Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.</strong></div>"; 
+    } else if ($_POST["password"] !== $_POST["password-confirm"]) {
+        echo "<strong><div>Passwords do not match.</strong></div>"; 
+    } else {
+        $options = [
+            'cost' => 11,
+        ];
+        $reset = $db->reset_password($response['id'], $response['username'], $response['email'], password_hash($_POST['password'], PASSWORD_BCRYPT, $options));
+        if($reset['status'] == 'success'){
+            header('Location: login?password=reset');
+        }
+    }
+}
+include('footer.php');
+?>
+ 
---
+++ live/addToFile.php
@@ -0,0 +1,75 @@
+<?php
+/** addToFile.php adds the "selectableBan"-Array on image click to a json file for live sharing on webpage
+ * 
+ * @author Florian Falk <dasnerdwork@gmail.com>
+ * @author Pascal Gnadt <p.gnadt@gmx.de>
+ * @copyright Copyright (c) date("Y"), Florian Falk
+ *
+ * @var string $championID The champion ID is the corresponding ingame used ID of a league champion, e.g. ID: MonkeyKing in the case of Name: Wukong
+ * @var string $championName The corresponding champion name of a league champion, e.g. Miss Fortune
+ * @var mixed $teamID Usually consisting of either numbers-only, numbers with dashes inbetween or a combination of letters, numbers and dashes, fetched from the URL
+ * @var string $currentPatch Current live patch grabbed as string from file (generated and checked by daily from patcher.py)
+ * @var array $validChamps The data parent element with all child elements of the champion.json, containing all necessary info for all current league champions
+ * @var mixed $searchedID Either the correct $championID or false if not found
+ * @var mixed $searchedName Either the correct $championName or false if not found
+ * @var array $preexistingBanFileContent Array formatted content of already generated file of selected bans for the team
+ * @var array $suggestBanArray Initialized as empty array but later on filled with necessary info for previous checks, either this array or the above can exist at once
+ * 
+ * Example data of $_POST from clash.js:
+ * $championID = "MonkeyKing";
+ * $championName = "Wukong";
+ * $teamID = "4892086";
+ */
+
+// Grabbing and referencing the posted variables + current patch as string
+$championID = $_POST["champid"];
+$championName = $_POST["champname"];
+$teamID = $_POST["teamid"];
+$currentPatch = file_get_contents("/var/www/html/clash/clashapp/data/patch/version.txt");
+
+// Array list of all valid champions, e.g. The champion Peter with ID: Peter and Name: Peter is invalid but Ashe with ID: Ashe and Name: Ashe is valid
+$validChamps = json_decode(file_get_contents('/var/www/html/clash/clashapp/data/patch/'.$currentPatch.'/data/de_DE/champion.json'), true)["data"];
+
+// Simple check if the url at least contains a teamID, as format from Riot changes no formatting check, just empty check
+if($teamID == "" || $teamID == "/"){
+    echo '{"status":"InvalidTeamID"}';die(); // Return via javascript json the InvalidTeamID status and stop further processing
+} else {
+    $searchedID = array_search($championID, array_column($validChamps, 'id'));
+    $searchedName = array_search($championName, array_column($validChamps, 'name'));
+    // Checks if both ID (key) and password (key2) are valid entries of the $valdiChamps array (champion.json entries)
+    if($searchedID !== $searchedName || $searchedID === false || $searchedName === false){
+        echo '{"status":"CodeInjectionDetected"}';die();
+    } else {
+        // If both ID and name are valid
+        if(file_exists('/var/www/html/clash/clashapp/data/teams/'.$teamID.'.json')){
+            // $preexistingBanFileContent = File content of preexisting savefile
+            $preexistingBanFileContent = json_decode(file_get_contents('/var/www/html/clash/clashapp/data/teams/'.$teamID.'.json'), true);
+            if(array_search($championID, array_column($preexistingBanFileContent["SuggestedBans"], 'id')) !== false){
+                echo '{"status":"ElementAlreadyInArray"}';die();
+            } else if (count($preexistingBanFileContent["SuggestedBans"]) >= 10) { // Maximum of 10 elements in #selectedBans
+                echo '{"status":"MaximumElementsExceeded"}';die();
+            } else {
+                echo '{"status":"Success"}';
+                // If all criterias are met open preexisting file for change (adding the entry to the file)
+                $fp = fopen('/var/www/html/clash/clashapp/data/teams/'.$teamID.'.json', 'c');
+                if($preexistingBanFileContent["SuggestedBans"] == $championID){ // Double check to make sure
+                    fclose($fp);
+                } else {
+                    $preexistingBanFileContent["SuggestedBans"][] = array("id"=>$championID,"name"=>$championName);
+                    $preexistingBanFileContent["Status"]++; // Increase status by 1 for every change made to the file (later on used by javascript in clash.js)
+                    fwrite($fp, json_encode($preexistingBanFileContent));
+                    fclose($fp);
+                }
+            }
+        } else {
+            $suggestBanArray = array(); // Initialize empty array to write into empty file
+            echo '{"status":"FileDoesNotExist"}'; // E.g. on first visit or file deletion -> File has to be created
+            $suggestBanArray["SuggestedBans"][] = array("id"=>$championID,"name"=>$championName);
+            $suggestBanArray["Status"] = 1; // Initial status of 1
+            $fp = fopen('/var/www/html/clash/clashapp/data/teams/'.$teamID.'.json', 'c');
+            fwrite($fp, json_encode($suggestBanArray));
+            fclose($fp);
+        }
+    }
+}
+?>
