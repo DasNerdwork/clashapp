b15bfeb4215171a33b8140a34b779b8717ca3d29
Florian Falk <dasnerdwork@gmail.com>
2022-12-23 01:05:57 +0100

    Testcommit 231222

--- patch-notes.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); 
-include('head.php');
-setCodeHeader('Patch Notes', true, true);
-include('header.php');
-echo "<div id='test'></div>";
-
-
-include('footer.php');
-?>
---
+++ scripts/watchTailwind.sh
@@ -0,0 +1,1 @@
+cd .. && screen -AmdS tailwindWatcher npx tailwindcss -i /hdd2/clashapp/css/clash.css -o /hdd2/clashapp/css/output.css --watch
