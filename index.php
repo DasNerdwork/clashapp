2f6f1a71bef282c273c8d4430bbf02a96497f401
Florian Falk <dasnerdwork@gmail.com>
2023-01-14 04:12:40 +0100

    Replaced javascript + ajax + php addToFile with WS

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include('/hdd2/clashapp/templates/head.php');
 setCodeHeader('Clash', true, true);
 include('/hdd2/clashapp/templates/header.php');
+
 if (!isset($_SESSION)) session_start();
 echo '
 <script>
@@ -11,5 +12,11 @@ document.body.style.backgroundPosition = "50% 20%";
 document.body.style.backgroundSize = "40%";
 </script>
 ';
+?>
+<script src="../clashapp/websocket.js"></script>
+
+
+
+<?php
 include('/hdd2/clashapp/templates/footer.php');
 ?>
