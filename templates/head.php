e6f9eecdfdba9096a93efbc4ca05c00dd3f81f6b
Florian Falk <dasnerdwork@gmail.com>
2023-01-22 03:45:38 +0100

    Fixed WS sometimes not loading bug, fixed QR Code for 2FA, added 404 traffic redirection and page, started splitting clash.js into multiple files

--- templates/head.php
+++ templates/head.php
@@ -11,8 +11,9 @@ function setCodeHeader($title, $css, $javascript, $alpinejs, $websocket, $favico
         // if($css){ echo '<link rel="stylesheet" href="/clashapp/clash.css">'; }
         if($css){ echo '<link rel="stylesheet" href="/clashapp/css/output.css">'; }
         if($alpinejs){ echo '<script src="//unpkg.com/alpinejs" defer></script>'; }
-        if($javascript){ echo '<script type="text/javascript" src="../clashapp/clash.min.js"></script>'; }
-        if($websocket){ echo '<script type="text/javascript" src="../clashapp/websocket.min.js"></script>'; }
+        if($javascript || $javascript == "qr"){ echo '<script type="text/javascript" src="../clashapp/clash.js"></script><script type="text/javascript" src="../clashapp/main.js"></script>'; }
+        if($javascript == "qr"){ echo '<script type="text/javascript" src="../clashapp/qr-codes.min.js"></script>'; }
+        if($websocket){ echo '<script type="text/javascript" src="../clashapp/websocket.js"></script>'; }
     echo '</head>';
 } // TODO: Websocket nur auf Team ID seiten aktivieren bzw. nur bestimmte websocket funktionalitäten
 ?>
