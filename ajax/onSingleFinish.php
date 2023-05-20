43062a654865f45a67391cd2f94ec5abea975f4c
DasNerdwork <dasnerdwork@gmail.com>
2023-05-20 04:08:28 +0200

    Added async playerlanes & avg match scores via xhr

--- ajax/onSingleFinish.php
+++ ajax/onSingleFinish.php
@@ -2,15 +2,16 @@
 
 // IF THIS COMES FROM UPDATE PHP ALSO HAVE TO JS ADD LANES
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 include_once('/hdd1/clashapp/functions.php');
 
 if(isset($_POST['mode'])){
     $matchids = array_keys(json_decode($_POST['matchids'], true));
     $path = $_POST['path']; // grab path from ajax request
+    $response = array('sumid' => substr($path, 0, -5));
     $recalculatedMatchIDsArray = array();
     // load players full match data into RAM array for fast access after page load
     $matchData = getMatchData($matchids); 
@@ -21,6 +22,7 @@ if(isset($_POST['mode'])){
         $puuid = $_POST['puuid']; // grab puuid from ajax request
         $playerLanes = getLanePercentages($matchData, $puuid); // Retrieves the two most played lanes of the give puuid
         $playerDataJSON["LanePercentages"] = $playerLanes;
+        $response['playerLanes'] = $playerDataJSON["LanePercentages"];
     } 
     
     if($_POST['mode'] == "both" || $_POST['mode'] == "scores"){
@@ -35,12 +37,15 @@ if(isset($_POST['mode'])){
             }
         }
         $playerDataJSON["MatchIDs"] = array_slice($recalculatedMatchIDsArray, 0, 15);
+        $response['matchScores'] = $playerDataJSON["MatchIDs"];
     }
     
     fwrite($fp, json_encode($playerDataJSON));
     fclose($fp);
-    
+
     unset($recalculatedMatchIDsArray); // Necessary to reset the array for the next player iteration, otherwise everyone has the same matchids stored
     unset($matchData); // Empty RAM
+
+    echo json_encode($response);
 }
 ?>
