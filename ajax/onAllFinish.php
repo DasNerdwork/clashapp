c09ad5f78644e4f55ab6c714e42c40b392ad3c57
DasNerdwork <dasnerdwork@gmail.com>
2023-06-10 22:07:00 +0200

    Refactoring der update.php

--- ajax/onAllFinish.php
+++ ajax/onAllFinish.php
@@ -2,6 +2,11 @@
 include_once('/hdd1/clashapp/functions.php');
 
 if(isset($_POST['allFinish'])){
-    
+    $suggestedBanMatchData = getMatchData($matchIDTeamArray);
+    $suggestedBanArray = getSuggestedBans(array_keys($playerSumidTeamArray), $masteryDataTeamArray, $playerLanesTeamArray, $matchIDTeamArray, $suggestedBanMatchData);
+    $currentTeamJSON["SuggestedBanData"] = $suggestedBanArray;
+    $fp = fopen('/hdd1/clashapp/data/teams/'.$teamID.'.json', 'w+');
+    fwrite($fp, json_encode($currentTeamJSON));
+    fclose($fp);
 }
 ?>
