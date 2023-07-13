2bb49b7465568df4817235c06b98ae076a77660b
DasNerdwork <dasnerdwork@gmail.com>
2023-07-14 01:48:42 +0200

    Added tests for matchdata

--- test/functionsTest.php
+++ test/functionsTest.php
@@ -115,12 +115,13 @@ class FunctionsTest extends TestCase {
     }
 
     public function testDownloadMatchesByID() {
+        $testMatchId = getMatchIDs("wZzROfU21vgztiGFq_trTZDeG89Q1CRGAKPktG83VKS-fkCISXhAWUptVVftbtVNIHMvgJo6nIlOyA", 1)[0];
         $downloadableFlag = false;
-        if(file_exists("/hdd1/clashapp/data/matches/EUW1_6480167518.json")) {
-            if(unlink("/hdd1/clashapp/data/matches/EUW1_6480167518.json")){
+        if(file_exists("/hdd1/clashapp/data/matches/".$testMatchId.".json")) {
+            if(unlink("/hdd1/clashapp/data/matches/".$testMatchId.".json")){
                 $downloadableFlag = true;
             } else {
-                echo "Unable to delete /hdd1/clashapp/data/matches/EUW1_6480167518.json";
+                echo "Unable to delete /hdd1/clashapp/data/matches/".$testMatchId.".json";
                 return;
             }
         } else {
@@ -128,9 +129,104 @@ class FunctionsTest extends TestCase {
         }
 
         if($downloadableFlag) {
-            $resultBoolean = downloadMatchesByID(["EUW1_6480167518"], "PHPUnit");
+            $resultBoolean = downloadMatchesByID([$testMatchId], "PHPUnit");
             $this->assertNotFalse($resultBoolean);
-            $this->assertFileExists("/hdd1/clashapp/data/matches/EUW1_6480167518.json", "Downloaded Match.json File does not exists/download correctly");
+            $this->assertFileExists("/hdd1/clashapp/data/matches/".$testMatchId.".json", "Downloaded Match.json File does not exists/download correctly");
+            $this->assertGreaterThan(0, filesize("/hdd1/clashapp/data/matches/".$testMatchId.".json"), "Downloaded Match.json File has filesize of 0");
+        }
+    }
+
+    public function testGetMatchData()
+    {
+        $testMatchId = getMatchIDs("wZzROfU21vgztiGFq_trTZDeG89Q1CRGAKPktG83VKS-fkCISXhAWUptVVftbtVNIHMvgJo6nIlOyA", 1)[0];
+        $matchData = (array) getMatchData([$testMatchId]);
+
+        $this->assertIsArray($matchData, "Fetched MatchData is no array");
+        $this->assertNotEmpty($matchData, "Fetched MatchData is empty");
+
+        $expectedKeys = [
+            'gameCreation',
+            'gameDuration',
+            'gameEndTimestamp',
+            'gameStartTimestamp',
+            'gameVersion',
+            'participants'
+        ];
+
+        $matchDataArray = (array) $matchData[$testMatchId];
+        $infoArray = (array) $matchData[$testMatchId]->info;
+        $participantArray = (array) $matchData[$testMatchId]->info->participants;
+    
+        // Validate the presence of expected keys
+        $missingKeys = array_diff($expectedKeys, array_keys($infoArray));
+        $this->assertEmpty($missingKeys, "MatchData info array is missing keys: " . implode(', ', $missingKeys));
+
+        // Validate there are no extra keys
+        $extraKeys = array_diff(array_keys($infoArray), $expectedKeys);
+        $this->assertEmpty($extraKeys, "MatchData info array has too many keys: " . implode(', ', $extraKeys));
+
+
+        // Make sure that these keys have successfully been removed from the array (unnecessary waste data)        
+        $this->assertArrayNotHasKey('metadata', $matchDataArray, 'The "metadata" attribute should not exist in $matchDataArray.');
+        $this->assertArrayNotHasKey('gameId', $infoArray, 'The "gameId" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('gameMode', $infoArray, 'The "gameMode" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('gameName', $infoArray, 'The "gameName" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('gameType', $infoArray, 'The "gameType" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('mapId', $infoArray, 'The "mapId" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('platformId', $infoArray, 'The "platformId" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('queueId', $infoArray, 'The "queueId" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('teams', $infoArray, 'The "teams" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('tournamentCode', $infoArray, 'The "tournamentCode" attribute should not exist in $infoArray.');
+        $this->assertArrayNotHasKey('allInPings', $participantArray, 'The "allInPings" attribute should not exist in $participantArray.');
+        $this->assertArrayNotHasKey('assistMePings', $participantArray, 'The "assistMePings" attribute should not exist in $participantArray.');
+        $this->assertArrayNotHasKey('baitPings', $participantArray, 'The "baitPings" attribute should not exist in $participantArray.');
+        $this->assertArrayNotHasKey('baronKills', $participantArray, 'The "baronKills" attribute should not exist in $participantArray.');
+        $this->assertArrayNotHasKey('basicPings', $participantArray, 'The "basicPings" attribute should not exist in $participantArray.');
+        $this->assertArrayNotHasKey('bountyLevel', $participantArray, 'The "bountyLevel" attribute should not exist in $participantArray.');
+
+        foreach ($participantArray as $participant) {
+            $expectedParticipantKeys = [
+                'assists',
+                'challenges',
+                'championName',
+                'championTransform',
+                'consumablesPurchased',
+                'damageDealtToBuildings',
+                'damageDealtToObjectives',
+                'damageSelfMitigated',
+                'deaths',
+                'detectorWardsPlaced',
+                'goldEarned',
+                'individualPosition',
+                'inhibitorTakedowns',
+                'kills',
+                'lane',
+                'neutralMinionsKilled',
+                'puuid',
+                'summonerId',
+                'summonerName',
+                'teamPosition',
+                'totalDamageDealtToChampions',
+                'totalDamageShieldedOnTeammates',
+                'totalDamageTaken',
+                'totalHealsOnTeammates',
+                'totalMinionsKilled',
+                'totalTimeCCDealt',
+                'totalTimeSpentDead',
+                'turretTakedowns',
+                'visionScore',
+                'wardsPlaced'
+            ];
+    
+            // Validate the presence of expected participant keys
+            $missingParticipantKeys = array_diff($expectedParticipantKeys, array_keys((array) $participant));
+            $this->assertEmpty($missingParticipantKeys, "MatchData participant array is missing keys: " . implode(', ', $missingParticipantKeys));
+
+            // Validate the presence of expected participant keys
+            $extraParticipantKeys = array_diff(array_keys((array) $participant), $expectedParticipantKeys);
+            unset($extraParticipantKeys[array_search('win', $extraParticipantKeys)]); // Exclude 'win' key from extra keys (broken but no idea why, maybe cuz of boolean)
+
+            $this->assertEmpty($extraParticipantKeys, "MatchData participant array has too many keys: " . implode(', ', $extraParticipantKeys));
         }
     }
 
