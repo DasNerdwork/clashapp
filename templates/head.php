09850cc3bfe6cc305f2f2734068ba737644e7f06
DasNerdwork <dasnerdwork@gmail.com>
2023-09-24 06:09:56 +0200

    Added autosuggest +  daily update + translation & dynamic display

--- templates/head.php
+++ templates/head.php
@@ -10,10 +10,10 @@ function setCodeHeader($title, $css, $javascript, $alpinejs, $websocket, $favico
         echo '<link id="favicon" rel="shortcut icon" href='.$favicon.'>';
         // if($css){ echo '<link rel="stylesheet" href="/clashapp/clash.css">'; }
         if($css){ echo '<link rel="stylesheet" href="/clashapp/css/output.css">'; }
-        if($alpinejs){ echo '<script src="//unpkg.com/alpinejs" defer></script>'; }
-        if($javascript || $javascript == "qr"){ echo '<script type="text/javascript" src="../clashapp/clash.min.js"></script><script type="text/javascript" src="../clashapp/main.js"></script>'; }
+        if($alpinejs){ echo '<script src="https://unpkg.com/alpinejs@3.13.0/dist/cdn.min.js" defer></script>'; }
+        if($javascript || $javascript == "qr"){ echo '<script type="text/javascript" src="../clashapp/main.min.js"></script><script type="text/javascript" src="../clashapp/clash.min.js"></script>'; }
         if($javascript === "qr"){ echo '<script type="text/javascript" src="../clashapp/qr-codes.min.js"></script>'; }
-        if($websocket){ echo '<script type="text/javascript" src="../clashapp/websocket.js"></script>'; }
+        if($websocket){ echo '<script type="text/javascript" src="../clashapp/websocket.min.js"></script>'; }
         echo '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8928684248089281" crossorigin="anonymous"></script>';
     echo '</head>';
 }
