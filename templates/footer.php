eba5452812cdd6d18dbf768f984fc0c4d0314f44
DasNerdwork <dasnerdwork@gmail.com>
2023-12-15 04:12:46 +0100

    Accessibility link color changes

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
     echo '<footer class="relative -bottom-2 w-full">';
 } ?>
     <div id="colophon" itemtype="https://schema.org/WPFooter" itemscope="itemscope" itemid="#colophon">
-        <div class="text-[#8984a5] text-center text-sm"><p class="text-center"> Copyright © 2022 - <?php echo date("Y"); ?> ClashScout.com | <a class="text-[#a2a2df] hover:text-[#d1d1fd]" href="https://dasnerdwork.net/impressum"><?= __("Impressum") ?></a> &amp; <a class="text-[#a2a2df] hover:text-[#d1d1fd]" href="https://dasnerdwork.net/Datenschutzerklaerung"><?= __("Datenschutzerklärung") ?></a></p>
+        <div class="text-[#8984a5] text-center text-sm"><p class="text-center"> Copyright © 2022 - <?php echo date("Y"); ?> ClashScout.com | <a class="text-white hover:text-[#d1d1fd]" href="https://dasnerdwork.net/impressum"><?= __("Impressum") ?></a> &amp; <a class="text-white hover:text-[#d1d1fd]" href="https://dasnerdwork.net/Datenschutzerklaerung"><?= __("Datenschutzerklärung") ?></a></p>
     </div>
     <div>
         <div class="text-[#8984a5] text-center text-sm"><?= __("ClashScout.com isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties.") ?>
