cf3d0f11cee65bac56fdbcc6e0df5fcc4f230495
DasNerdwork <dasnerdwork@gmail.com>
2024-01-11 03:27:18 +0100

    Added csrf tokens for safety on js return ajax requests

--- team.php
+++ team.php
@@ -1,5 +1,6 @@
 <?php 
 if (session_status() === PHP_SESSION_NONE) session_start();
+$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
