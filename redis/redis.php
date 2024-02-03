7ea5f77ce894f37a6e21e2b7d2b875e39512660e
DasNerdwork <dasnerdwork@gmail.com>
2024-02-03 01:27:10 +0100

    Fixes & Rate Limit Enforcing

---
+++ redis/redis.php
@@ -0,0 +1,18 @@
+<?php
+require '/hdd1/clashapp/vendor/autoload.php';
+
+// Verbindung zu Redis herstellen (Standardkonfiguration: ***REMOVED***, Port 6379)
+$redis = new Predis\Client();
+
+// Funktion zum Hinzufügen von Anfragen zur Warteschlange
+function addToQueue($queueName, $type, $data) {
+    global $redis;
+    $timestamp = microtime();
+    $item = [
+        'type' => $type,
+        'timestamp' => $timestamp,
+        'data' => $data,
+    ];
+    $redis->rpush($queueName, json_encode($item));
+    return $timestamp;
+}
--- update.php
+++ update.php
@@ -42,13 +42,17 @@ function updateProfile($id, $teamID, $type="riot-id", $tempMatchIDs=null){
         $returnScriptContent = "";
         $mdb = new MongoDBHelper();
         $playerData = getPlayerData($type,$id);
+        addToQueue('api_queue', 'playerData', ['type' => $type, 'id' => $id]);
         $playerName = $playerData["GameName"];
         $sumid = $playerData["SumID"];
         $puuid = $playerData["PUUID"];
         $masteryData = getMasteryScores($puuid);
+        addToQueue('api_queue', 'masteryScores', ['puuid' => $puuid]);
         $rankData = getCurrentRank($sumid);
+        addToQueue('api_queue', 'currentRank', ['sumid' => $sumid]);
         if($tempMatchIDs == null){
             $matchIDs = getMatchIDs($puuid, 15);
+            addToQueue('api_queue', 'matchIds', ['puuid' => $puuid, 'maxMatchIDs' => 15]);
         } else {
             $matchIDs = $tempMatchIDs;
         }
